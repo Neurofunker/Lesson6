2cbe902fce7751f9e22ad25e8688582d6eadaedc
Makc <m_stefanenkov@mail.ru>
2023-02-18 17:27:07 +0300

    Решили задачу 40

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -13,11 +13,9 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 
 bool TriangCompare(int num1, int num2, int num3)
 {
-    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2)
-    {
-        return true;
-    }
-else return false;
+    return (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2);
+    
+
 }
 bool result = TriangCompare(number1, number2, number3);
-Console.WriteLine(result);
+Console.WriteLine(result  ? "Да" : "Нет");
